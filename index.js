917cbfa8cd7fc38af2301b142220231251d1a045
programmerniki <nk5564633@gmail.com>
2025-08-05 18:48:18 +0530

    add env

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import userRout from "./routs/userRouts.js"
+import dotenv from "dotenv";
+import userRout from "./routs/userRouts.js";
 const app = express();
+dotenv.config();
 app.use(cors());
 app.use(express.json());
-const PORT = 5000 || 5001;
- mongoose.connect(
-    "mongodb+srv://kmnikita0000200:Nikita9569@cluster0.piztj59.mongodb.net/?"
-  ).then(()=>console.log("mongodb connect successfull")).catch((err)=>console.log(`error-${err}`))
 
- 
-app.use('/api',userRout)
+const PORT = process.env.PORT || 5001;
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => console.log("mongodb connect successfull"))
+  .catch((err) => console.log(`error-${err}`));
+
+app.use("/api", userRout);
 app.listen(PORT, () => {
   console.log(`server is running localhost ${PORT}`);
 });
